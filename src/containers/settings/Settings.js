d93493b93c74dfeeed11e62215c0db900df2b0c5
Afra Arslan <akbasafra.aa@gmail.com>
2020-11-01 16:00:51 +0300

    edit settings page

--- src/containers/settings/Settings.js
+++ src/containers/settings/Settings.js
@@ -1,10 +1,52 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React from 'react';
+import {View, Text} from 'react-native';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 export default function SettingsScreen() {
-    return (
-        <View style={{ flex: 1, justifyContent: "center", alignContent: "center"}}>
-            <Text> Settings </Text>
+  return (
+    <SafeAreaView
+      style={{flexGrow: 1, backgroundColor: 'black'}}
+      edges={['top']}>
+      <View style={{height: 50, flexDirection: 'row'}}>
+        <View
+          style={{
+            flex: 1,
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}>
+          <Text
+            style={{
+              fontSize: 24,
+              color: '#3fc295',
+              fontWeight: 'bold',
+            }}>
+            {' '}
+            Settings{' '}
+          </Text>
         </View>
-    )
+      </View>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: 'red',
+        }}>
+        <View style={{height: 90, padding: 10}}>
+          <Text style={{fontSize: 20, fontWeight: 'bold', color: 'white'}}>
+            Your Profile
+          </Text>
+        </View>
+      </View>
+      <View
+        style={{
+          flex: 2,
+          backgroundColor: 'green',
+        }}>
+        <View style={{height: 90, padding: 10}}>
+          <Text style={{fontSize: 20, fontWeight: 'bold', color: 'white'}}>
+            Setting Options
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
+  );
 }
